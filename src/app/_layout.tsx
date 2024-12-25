a367fe3c68ca182e265b28dca69ad15f5de620d9
tilo1306 <doug1306@gmail.com>
2024-12-25 18:52:15 -0300

    chore: Ajust path

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,7 @@
 import { Text } from 'react-native';
 
 import "@/global.css";
-import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
+import { GluestackUIProvider } from './components/ui/gluestack-ui-provider';
 
 export default function RootLayout() {
   return <GluestackUIProvider mode="light"><Text>oi</Text></GluestackUIProvider>;
